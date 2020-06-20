996a517c7a4a07ea366da802d6a54690d565762f
Simon Goldin <simon.goldin@dashlane.com>
2020-06-20 17:42:17 -0400

    fixing mobile

--- components/BadKeyboard/Form.tsx
+++ components/BadKeyboard/Form.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { noop } from 'lodash';
 import styled from '@emotion/styled';
 import { Theme } from '../../styles/defaultTheme';
 import { BACKSPACE } from './Keyboard';
+import { useTheme } from 'emotion-theming';
 
 const StyledForm = styled.form(({ theme }: { theme: Theme }) => ({
   borderWidth: 1,
@@ -17,13 +19,19 @@ const StyledForm = styled.form(({ theme }: { theme: Theme }) => ({
 }));
 
 const StyledInput = styled.input(({ theme }: { theme: Theme }) => ({
-  outline: 'none',
+  color: 'transparent',
+  textShadow: `0 0 0 ${theme.colors.gray.dark}`,
   fontSize: theme.dimensions.fontSize.regular,
   lineHeight: theme.dimensions.lineHeight.regular * 1.5,
-  letterSpacing: 2,
-  borderWidth: 1,
+  letterSpacing: 1,
   borderColor: theme.colors.gray.calm,
-  cursor: 'not-allowed'
+  borderRadius: 8,
+  borderStyle: 'solid',
+  borderWidth: 2,
+  cursor: 'not-allowed',
+  '&:focus': {
+    outline: 'none'
+  }
 }));
 
 const StyledButton = styled.button(({ theme }: { theme: Theme }) => ({
@@ -39,32 +47,26 @@ type FormValues = {
 };
 
 type OnDrop = (
-  e: React.DragEvent<HTMLInputElement>,
+  text: string,
   valueKey: keyof FormValues,
   values: FormValues,
-  setValues: React.Dispatch<React.SetStateAction<FormValues>>,
-  setReadOnly: React.Dispatch<React.SetStateAction<boolean>>
+  setValues: React.Dispatch<React.SetStateAction<FormValues>>
 ) => void;
 
-const updateForm: OnDrop = (e, valueKey, values, setValues, setReadOnly) => {
-  const { dataTransfer } = e;
-
-  const text = dataTransfer.getData('text');
-
+const updateForm: OnDrop = (text, valueKey, values, setValues) => {
   setValues((oldValues) => {
     // for now, just append to the end.
-    // it seems impossible to know at what selection index the drop is occurring before it happens.
+    // it seems impossible to know at what selection index the drop is occurring before it happens.2
     // selectionStart and selectionEnd are not updated as the drag is happening.
     const newVal = text === BACKSPACE ? oldValues[valueKey].slice(0, -1) : oldValues[valueKey] + text;
 
     return { ...oldValues, [valueKey]: newVal };
   });
-  setTimeout(() => {
-    setReadOnly(true);
-  }, 0);
 };
 
 export const Form = () => {
+  const theme = useTheme<Theme>();
+
   const onSubmit = (values: FormValues) => {
     const message = Object.keys(values).reduce(
       (acc, currentKey) => [...acc, `\n${currentKey}: ${values[currentKey as keyof FormValues].toString()}`],
@@ -74,14 +76,20 @@ export const Form = () => {
   };
 
   const [values, setValues] = React.useState<FormValues>({ value1: '', value2: '' });
-  const [readOnly, setReadOnly] = React.useState(true);
-
-  const onDragOver: React.DragEventHandler<HTMLInputElement> = () => {
-    setReadOnly(false);
-  };
 
   const onDrop = (e: React.DragEvent<HTMLInputElement>, valueKey: keyof FormValues) => {
-    updateForm(e, valueKey, values, setValues, setReadOnly);
+    const { dataTransfer } = e;
+    const text = dataTransfer.getData('text');
+
+    updateForm(text, valueKey, values, setValues);
+  };
+
+  const setHighlight = ({ currentTarget }: React.DragEvent<HTMLElement>) => {
+    currentTarget.style.borderColor = theme.colors.accent;
+  };
+
+  const clearHighlight = ({ currentTarget }: React.DragEvent<HTMLElement>) => {
+    currentTarget.style.borderColor = '';
   };
 
   return (
@@ -96,18 +104,20 @@ export const Form = () => {
       <StyledInput
         placeholder="Drag here"
         type="text"
-        onDragOver={onDragOver}
+        onDragEnter={setHighlight}
+        onDragLeave={clearHighlight}
         onDrop={(e) => onDrop(e, 'value1')}
         value={values.value1}
-        readOnly={readOnly}
+        onChange={noop}
       />
       <StyledInput
-        placeholder="Drag here"
+        placeholder="Or drag here"
         type="text"
-        onDragOver={onDragOver}
+        onDragEnter={setHighlight}
+        onDragLeave={clearHighlight}
         onDrop={(e) => onDrop(e, 'value2')}
         value={values.value2}
-        readOnly={readOnly}
+        onChange={noop}
       />
       <StyledButton type="submit">Submit</StyledButton>
     </StyledForm>
