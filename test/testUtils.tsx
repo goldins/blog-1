32f716f8ac0082390a9c6ee970de4a6edba5c53a
Simon Goldin <simon.goldin@dashlane.com>
2020-07-01 23:00:01 -0400

    cleanup!

--- test/testUtils.tsx
+++ test/testUtils.tsx
@@ -2,24 +2,13 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'emotion-theming';
 import { defaultTheme } from '../styles/defaultTheme';
-// import { TranslationProvider } from "my-i18n-lib"
-// import defaultStrings from "i18n/en-x-default"
 
 const Providers = ({ children }: React.PropsWithChildren<unknown>): React.ReactElement => {
   return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>;
-  // return (
-  //   <ThemeProvider theme="light">
-  //     <TranslationProvider messages={defaultStrings}>
-  //       {children}
-  //     </TranslationProvider>
-  //   </ThemeProvider>
-  // )
 };
 
 const customRender = (ui: React.ReactElement, options = {}) => render(ui, { wrapper: Providers, ...options });
 
-// re-export everything
 export * from '@testing-library/react';
 
-// override render method
 export { customRender as render };
