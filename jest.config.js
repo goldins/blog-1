ff1b926b25eddc0bb7d21ed566f115c41d4e9f16
Simon Goldin <simon.goldin@dashlane.com>
2020-08-02 17:10:14 -0400

    moving stuff around; prettier @ 100 printWidth

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js'
   },
-  setupFilesAfterEnv: ['./jest.setup.ts']
+  setupFilesAfterEnv: ['./test/jest.setup.ts']
 };
