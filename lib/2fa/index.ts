fffce3895613940c9e5cb5784f79233eec7ba052
Simon Goldin <simon.goldin@dashlane.com>
2020-08-09 16:24:18 -0400

    better token generation

--- lib/2fa/index.ts
+++ lib/2fa/index.ts
@@ -30,9 +30,7 @@ export const generateToken = (secret: string, counter: number) => {
     ((digest[offset + 2] & 0xff) << 8) |
     (digest[offset + 3] & 0xff);
 
-  // max code: 0x7fffffff or 2147483647
-  const scale = 0x7fffffff / EMOJI_COUNT;
+  const emojiIndex = Math.floor(code % EMOJI_COUNT);
 
-  const emojiIndex = Math.floor(code / scale);
   return EMOJIS[emojiIndex];
 };
